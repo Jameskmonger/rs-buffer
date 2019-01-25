1fd7b6a37b53e943845baeb797fa45250bef634e
James Monger <jameskmonger@hotmail.co.uk>
2019-01-25 23:45:17 +0000

    Implement readShort transformations

--- src/readable-byte-buffer.ts
+++ src/readable-byte-buffer.ts
@@ -40,15 +40,15 @@ export class ReadableByteBuffer {
             : transformed & 0xFF;
     }
 
-    public readShort(signed: boolean = true): number {
-        const val = (
-            (this.getNextFromBuffer() << 8) +
-            (this.getNextFromBuffer() & 0xFF)
-        );
+    public readShort(signed: boolean = true, transformation: Transformation = Transformation.NONE): number {
+        const msb = this.getNextFromBuffer();
+        const lsb = reverseTransformation(this.getNextFromBuffer(), transformation);
+
+        const result = (msb << 8) + (lsb & 0xFF);
 
         return signed
-            ? wrapNumber(val, -0x8000, 0x7FFF)
-            : val & 0xFFFF;
+            ? wrapNumber(result, -0x8000, 0x7FFF)
+            : result & 0xFFFF;
     }
 
     public readTribyte(signed: boolean = true): number {
--- test/readable-byte-buffer/read-short.test.ts
+++ test/readable-byte-buffer/read-short.test.ts
@@ -1,8 +1,7 @@
-import { TestFixture, TestCase, Expect, FocusTests } from "alsatian";
+import { TestFixture, TestCase, Expect, FocusTests, FocusTest } from "alsatian";
 
-import { ReadableByteBuffer } from "../../src/";
+import { ReadableByteBuffer, Transformation } from "../../src/";
 
-@FocusTests
 @TestFixture("ByteBuffer#readShort tests")
 export class ByteBufferReadShortTestFixture {
 
@@ -47,6 +46,34 @@ export class ByteBufferReadShortTestFixture {
         const output = [
             buffer.readShort(true),
             buffer.readShort(true)
+        ];
+
+        Expect(output).toEqual(expected);
+    }
+
+    @TestCase([0x12, 0x34], Transformation.NONE, 0x1234)
+    @TestCase([0x01, -0x7E], Transformation.ADD, 0x0102)
+    @TestCase([0x33, 0x3B], Transformation.SUBTRACT, 0x3345)
+    @TestCase([0x77, -0x67], Transformation.NEGATE, 0x7767)
+    public shouldReadUnsignedShortWithCorrectTransformation(input: Array<number>, transform: Transformation, expected: number) {
+        const buffer = ReadableByteBuffer.fromArray(input);
+
+        const output = [
+            buffer.readShort(false, transform)
+        ];
+
+        Expect(output).toEqual(expected);
+    }
+
+    @TestCase([-0x35, 0x55], Transformation.NONE, -0x34AB)
+    @TestCase([-0x35, -0x2b], Transformation.ADD, -0x34AB)
+    @TestCase([-0x35, 0x2b], Transformation.SUBTRACT, -0x34AB)
+    @TestCase([-0x35, -0x55], Transformation.NEGATE, -0x34AB)
+    public shouldReadSignedShortWithCorrectTransformation(input: Array<number>, transform: Transformation, expected: number) {
+        const buffer = ReadableByteBuffer.fromArray(input);
+
+        const output = [
+            buffer.readShort(true, transform)
         ];
 
         Expect(output).toEqual(expected);
