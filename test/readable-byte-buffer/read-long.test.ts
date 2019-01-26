1cdc39f50b389c8c543f5318b90f587c489b7cb3
James Monger <jameskmonger@hotmail.co.uk>
2019-01-26 12:54:38 +0000

    Make long read unsigned

--- test/readable-byte-buffer/read-long.test.ts
+++ test/readable-byte-buffer/read-long.test.ts
@@ -1,23 +1,18 @@
-import { TestFixture, TestCase, Expect, FocusTest, IgnoreTest, IgnoreTests } from "alsatian";
-import * as Long from "long";
+import { TestFixture, TestCase, Expect, FocusTest, IgnoreTest, IgnoreTests, FocusTests } from "alsatian";
 
 import { ReadableByteBuffer } from "../../src/";
 
-@IgnoreTests("These need to be fixed")
 @TestFixture("ByteBuffer#readLong tests")
 export class ByteBufferReadLongTestFixture {
 
-    @TestCase([ 0x12, 0x34, 0x56, 0x78, 0x71, 0xB2, 0xC3, 0xD4 ], "0x1234567871B2C3D4")
-    @TestCase([ 0x12, 0x34, 0x56, 0x78, 0x71, -0x4E, -0x3D, -0x2C ], "0x1234567871B2C3D4")
-    @TestCase([ -0x5B, 0x44, 0x33, 0x22, -0x1C, -0x2E, -0x40, -0x4E ], "-0x5ABBCCDD1B2D3F4E")
-    public shouldReadLong(input: number[], expected: string) {
-        const expectedLong = Long.fromString(expected, 16);
-
+    @TestCase([ 0x12, 0x34, 0x56, 0x78, 0x71, -0x4E, -0x3D, -0x2C ], [ 0x12345678, 0x71B2C3D4 ])
+    @TestCase([ -0x5B, 0x44, 0x33, 0x22, -0x1C, -0x2E, -0x40, -0x4E ], [ 0xA5443322, 0xE4D2C0B2 ])
+    public shouldReadLong(input: number[], expected: [number, number]) {
         const buffer = ReadableByteBuffer.fromArray(input);
 
         const output = buffer.readLong();
 
-        Expect(output).toEqual([ expectedLong.getHighBits(), expectedLong.getLowBits() ]);
+        Expect(output).toEqual(expected);
     }
 
 }
