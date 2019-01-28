cfdce628719ca29161c392b5df33570df909cac0
James Monger <jameskmonger@hotmail.co.uk>
2019-01-28 16:48:02 +0000

    Add order tests to ReadShort

--- test/readable-byte-buffer/read-short.test.ts
+++ test/readable-byte-buffer/read-short.test.ts
@@ -1,26 +1,30 @@
 import { TestFixture, TestCase, Expect, FocusTests, FocusTest } from "alsatian";
 
-import { ReadableByteBuffer, Transformation } from "../../src/";
+import { ReadableByteBuffer, Transformation, DataOrder } from "../../src/";
 
 @TestFixture("ByteBuffer#readShort tests")
 export class ByteBufferReadShortTestFixture {
 
-    @TestCase([ 0x2F, -0x01 ], 0x2FFF)
-    @TestCase([ -0x56, -0x45 ], 0xAABB)
-    public shouldReadUnsignedShort(input: number[], expected: number) {
+    @TestCase([ 0x2F, -0x01 ], DataOrder.BIG_ENDIAN, 0x2FFF)
+    @TestCase([ -0x56, -0x45 ], DataOrder.BIG_ENDIAN, 0xAABB)
+    @TestCase([ -0x01, 0x2F ], DataOrder.LITTLE_ENDIAN, 0x2FFF)
+    @TestCase([ -0x45, -0x56 ], DataOrder.LITTLE_ENDIAN, 0xAABB)
+    public shouldReadUnsignedShort(input: number[], order: DataOrder, expected: number) {
         const buffer = ReadableByteBuffer.fromArray(input);
 
-        const output = buffer.readShort(false);
+        const output = buffer.readShort(false, Transformation.NONE, order);
 
         Expect(output).toBe(expected);
     }
 
-    @TestCase([ 0x12, 0x34 ], 0x1234)
-    @TestCase([ -0x5B, 0x45 ], -0x5ABB)
-    public shouldReadSignedShort(input: number[], expected: number) {
+    @TestCase([ 0x12, 0x34 ], DataOrder.BIG_ENDIAN, 0x1234)
+    @TestCase([ -0x5B, 0x45 ], DataOrder.BIG_ENDIAN, -0x5ABB)
+    @TestCase([ 0x34, 0x12 ], DataOrder.LITTLE_ENDIAN, 0x1234)
+    @TestCase([ 0x45, -0x5B ], DataOrder.LITTLE_ENDIAN, -0x5ABB)
+    public shouldReadSignedShort(input: number[], order: DataOrder, expected: number) {
         const buffer = ReadableByteBuffer.fromArray(input);
 
-        const output = buffer.readShort(true);
+        const output = buffer.readShort(true, Transformation.NONE, order);
 
         Expect(output).toBe(expected);
     }
@@ -48,6 +52,18 @@ export class ByteBufferReadShortTestFixture {
 
         const output = [
             buffer.readShort(true, transform)
+        ];
+
+        Expect(output).toEqual(expected);
+    }
+
+    @TestCase([0x3B, 0x33], Transformation.SUBTRACT, 0x3345)
+    @TestCase([-0x67, 0x77], Transformation.NEGATE, 0x7767)
+    public shouldApplyTransformationCorrectlyInLittleEndianOrder(input: Array<number>, transform: Transformation, expected: number) {
+        const buffer = ReadableByteBuffer.fromArray(input);
+
+        const output = [
+            buffer.readShort(false, transform, DataOrder.LITTLE_ENDIAN)
         ];
 
         Expect(output).toEqual(expected);
